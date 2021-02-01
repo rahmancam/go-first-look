f5a99aed88e24ce8f17252d40810bc05cddcb877
Abdul Rahman <rahmancam@live.com>
2021-02-01 13:02:00 +0530

    Add variables example

---
+++ variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var globalName string = "John"
+var name1, name2 = "Peter", "Parker"
+
+func main() {
+	var name string = "Abdul Rahman"
+	var surName string
+	var someNumber int
+	name3 := "Will"
+	fmt.Println(name)
+	fmt.Println(surName)
+	fmt.Println(someNumber)
+	fmt.Println(globalName)
+	fmt.Println(name1, name2)
+	fmt.Println(name3)
+}
